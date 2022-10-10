46fd690a52b14723eb345fd36f65d7d3613f0a15
jovijovi <mageyul@hotmail.com>
2022-10-10 18:42:12 +0800

    feat: load module by module loader

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,14 +4,13 @@ import {network} from '@jovijovi/ether-network';
 import {customConfig} from '../config';
 import {logo} from './logo';
 import {RestfulHandlers} from '../handler';
-import {crawler} from '../crawler';
+import {module} from '../module';
 
 function main() {
 	log.logo(logo);
 	sys.HandleSignals();
 	config.LoadConfig();
 	customConfig.LoadCustomConfig();
-	http.server.Run(RestfulHandlers);
 
 	network.LoadConfig(customConfig.Get());
 	network.isConnected().then(r => {
@@ -20,10 +19,9 @@ function main() {
 		}
 	});
 
-	crawler.Run().catch((err) => {
-		log.RequestId().fatal(err);
-		sys.Shutdown();
-	});
+	module.Load();
+
+	http.server.Run(RestfulHandlers);
 }
 
 main();
---
+++ src/module/id.ts
@@ -0,0 +1,1 @@
+export const ModuleCrawler = 'crawler';
